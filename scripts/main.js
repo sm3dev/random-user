93d2235237258a976625bce6ac1aae8132c40b70
Michael Wright <info@saymoremultimedia.com>
2021-09-07 09:44:22 -0500

    created the function in main.js and got it to pull random users and console.log their complete info

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,16 @@
 console.log("You look marvelous!")
+
+const showRandomUser = () => {
+
+    return fetch("https://randomuser.me/api/").then(response => response.json()).then(parsedResponse => {
+        console.log(parsedResponse);
+    })
+    // url: 'https://randomuser.me/api/',
+    // dataType: 'json',
+    // success: function(data) {
+    //   console.log(data);
+    // }
+};
+
+showRandomUser();
+     
